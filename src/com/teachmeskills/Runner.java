05093ba27ff1433a6a8329dcd0f4baa0e618ca2a
monichkirill <kirillmonich13@icloud.com>
2024-12-01 20:35:02 +0300

    done

--- src/com/teachmeskills/Runner.java
+++ src/com/teachmeskills/Runner.java
@@ -1,4 +1,37 @@
 package com.teachmeskills;
 
+import com.teachmeskills.student.Student;
+
+import java.util.*;
+
 public class Runner {
+    public static void main(String[] args) {
+        Set<String> passportNumbers = new HashSet<>(Arrays.asList("MH1234567", "MC2345678", "CD3456789"));
+
+        // Коллекция студентов
+        Set<Student> students = new HashSet<>();
+        students.add(new Student("Ivan Ivanov", 20));
+        students.add(new Student("Anna Petrova", 21));
+        students.add(new Student("Pavel Sidorov", 22));
+
+        // Проверка на равное количество элементов
+        if (passportNumbers.size() != students.size()) {
+            System.out.println("The number of passport numbers does not match the number of students!");
+            return;
+        }
+
+        // Создание коллекции ключ-значение
+        Iterator<String> passportIterator = passportNumbers.iterator();
+        Iterator<Student> studentIterator = students.iterator();
+
+        Map<String, Student> passportToStudentMap = new HashMap<>();
+        while (passportIterator.hasNext() && studentIterator.hasNext()) {
+            passportToStudentMap.put(passportIterator.next(), studentIterator.next());
+        }
+
+        // Вывод коллекции ключ-значение
+        for (Map.Entry<String, Student> entry : passportToStudentMap.entrySet()) {
+            System.out.println("Passport: " + entry.getKey() + ", Student: " + entry.getValue());
+        }
+    }
 }
